9470b12995617dddc07dfdb8a1846fbf95526774
claydotd <claydleslie@icloud.com>
2026-07-13 08:32:12 +0100

    Change vite config to rollup index and 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // Set base to your GitHub repo name for GitHub Pages deployment.
 // Use '/' if deploying to a custom domain or username.github.io root.
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "404.html"),
+      },
+    },
+  },
 })
