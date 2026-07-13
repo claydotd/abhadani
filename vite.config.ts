0bb5889a527e03f93973e78b752ca9769d1bbd7f
claydotd <claydleslie@icloud.com>
2026-07-13 11:17:49 +0100

    vite config update for CNAME

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         404: resolve(__dirname, "404.html"),
+        cname: resolve(__dirname,"CNAME"),
       },
     },
   },
